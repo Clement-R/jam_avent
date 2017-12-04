381d07544c7ea72d5d553eacf7cfce5795a903ee
C.Rondeau <rondeau.clement@gmail.com>
2017-12-04 20:39:13 +0100

    Add sprites and backgrounds for game 4

---
+++ Assets/TickManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TickManager : MonoBehaviour {
+
+    [SerializeField]
+    private int _ratePerSecond = 10;
+
+    void Start ()
+    {
+		
+	}
+	
+	void Update ()
+    {
+        int ratePerMs = _ratePerSecond / (int) (Time.fixedDeltaTime * 1000);
+        // print(Time.fixedDeltaTime * 1000);
+        print(ratePerMs);
+    }
+}
