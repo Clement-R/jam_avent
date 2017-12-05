676e5b0f307ccf3c01f83315adfc66103ef41404
Clement-R <rondeau.clement@gmail.com>
2017-12-05 16:02:31 +0100

    Start to work on game 5

---
+++ Assets/Game5_FloorBehavior.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game5_FloorBehavior : MonoBehaviour {
+    
+    private void OnCollisionEnter2D(Collision2D collider)
+    {
+        if(collider.gameObject.tag == "PickUp")
+        {
+            print("Lose");
+        }
+    }
+}
--- Assets/Game5_GameController.cs
+++ Assets/Game5_GameController.cs
@@ -4,13 +4,21 @@ using UnityEngine;
 
 public class Game5_GameController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    public GameObject[] presents;
+    public Sprite[] presentSprites;
+
+    void Start ()
+    {
 		
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+    {
+		if(Input.GetMouseButtonDown(0))
+        {
+            print("Hello world");
+            Vector2 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            GameObject go = Instantiate(presents[Random.Range(0, presents.Length)], pos, Quaternion.identity);
+        }
 	}
 }
