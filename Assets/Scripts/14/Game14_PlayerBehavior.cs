1096540ac3c5e51a9636176f56d6339cc2abba21
MarieLouB <marielou.boisaubert@gmail.com>
2017-12-10 15:57:13 +0100

    Beginning of game 14 (jump, move, platforms)

---
+++ Assets/Scripts/14/Game14_PlayerBehavior.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game14_PlayerBehavior : MonoBehaviour {
+
+    public float movementSpeed;
+
+    private float movement = 0f;
+
+    Rigidbody2D rb;
+	
+	void Start ()
+    {
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	void Update ()
+    {
+        movement = Input.GetAxis("Horizontal") * movementSpeed;
+	}
+
+    private void FixedUpdate()
+    {
+        Vector2 velocity = rb.velocity;
+        velocity.x = movement;
+        rb.velocity = velocity;
+    }
+}
