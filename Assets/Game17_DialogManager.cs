d0671a29e398f408e5b63a2db32e7d87787dbc50
C.Rondeau <rondeau.clement@gmail.com>
2017-12-21 18:12:35 +0100

    Game 17 added and working on game 18

---
+++ Assets/Game17_DialogManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Game17_DialogManager : MonoBehaviour {
+
+    public string[] messages;
+    public float clickCooldown = 2f;
+    public Text message;
+
+    public GameObject button1;
+    public GameObject button2;
+
+    private int _messageIndex = 0;
+    private float _nextMessage = 0f;
+	
+	void Start ()
+    {
+        message.text = messages[_messageIndex];
+    }
+
+    public void NextMessage()
+    {
+        if(Time.time >= _nextMessage)
+        {
+            if (_messageIndex + 1 == messages.Length - 1)
+            {
+                StartCoroutine(EndGame());
+            }
+            else
+            {
+                // Display new message
+                _messageIndex++;
+                message.text = messages[_messageIndex];
+                _nextMessage = Time.time + clickCooldown;
+            }
+        }
+    }
+
+    IEnumerator EndGame()
+    {
+        // Show last message
+        _messageIndex++;
+        message.text = messages[_messageIndex];
+        // Hide buttons
+        button1.SetActive(false);
+        button2.SetActive(false);
+        yield return new WaitForSeconds(clickCooldown);
+        // TODO : Game end panel
+    }
+}
---
+++ Assets/Game18_GameManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Game18_GameManager : MonoBehaviour {
+
+    public Text numberOfElves;
+
+    [SerializeField]
+    private int _ratePerSecond = 10;
+    [SerializeField]
+    private float _elfPerClick = 1;
+    private float _ratePerMs = 0;
+    private float _numberOfElf = 0;
+
+    void Start ()
+    {
+        ComputeRatePerMs();
+        StartCoroutine(Up());
+    }
+
+    void ComputeRatePerMs()
+    {
+        _ratePerMs = _ratePerSecond / 1000f;
+        print(_ratePerMs);
+    }
+
+    IEnumerator Up()
+    {
+        _numberOfElf += (int)Mathf.Ceil(_ratePerMs * Time.deltaTime);
+        numberOfElves.text = _numberOfElf.ToString();
+        yield return null;
+        StartCoroutine(Up());
+    }
+
+    public void Click()
+    {
+        _numberOfElf += _elfPerClick;
+    }
+}
