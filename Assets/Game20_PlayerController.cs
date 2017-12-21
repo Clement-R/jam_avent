f9a8526ea274919e62370546f5ecc0cc53dac35a
C.Rondeau <rondeau.clement@gmail.com>
2017-12-21 21:48:17 +0100

    Working on mechanics of game 20

---
+++ Assets/Game20_PlayerController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game20_PlayerController : MonoBehaviour {
+
+    public bool IsInSight = false;
+    public bool IsInPresentRange = false;
+
+    [SerializeField]
+    private float _hMaxSpeed = 175f;
+    [SerializeField]
+    private float _vMaxSpeed = 150f;
+
+    private Game20_PresentBehavior _present;
+    private Rigidbody2D _rb;
+
+    void Start () {
+        _rb = GetComponent<Rigidbody2D>();
+        _present = GameObject.FindGameObjectWithTag("PickUp").GetComponent<Game20_PresentBehavior>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && IsInPresentRange)
+        {
+            if(IsInSight)
+            {
+                print("Lose");
+            }
+            else
+            {
+                print("MASH");
+                _present.Smash();
+            }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        // Movement
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+        _rb.velocity = Vector2.Scale(new Vector2(h, v), new Vector2(_hMaxSpeed, _vMaxSpeed));
+    }
+}
---
+++ Assets/Game20_PresentBehavior.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game20_PresentBehavior : MonoBehaviour {
+
+    [SerializeField]
+    private int _health = 100;
+    private Game20_PlayerController _player;
+
+	// Use this for initialization
+	void Start () {
+        _player = GameObject.Find("Player").GetComponent<Game20_PlayerController>();
+    }
+	
+	public void Smash()
+    {
+        _health -= 1;
+        // TODO : Manage different states of the present
+
+        if(_health == 0)
+        {
+            // TODO : end game
+            print("Win");
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            _player.IsInPresentRange = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            _player.IsInPresentRange = false;
+        }
+    }
+}
