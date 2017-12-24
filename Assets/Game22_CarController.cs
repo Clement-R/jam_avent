5ce90bf01b73ace5f2c5bcf212725564092684fa
Clement-R <rondeau.clement@gmail.com>
2017-12-24 18:46:41 +0100

    Finish game 22

--- Assets/Game22_CarController.cs
+++ Assets/Game22_CarController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Game22_CarController : MonoBehaviour {
 
@@ -12,33 +13,58 @@ public class Game22_CarController : MonoBehaviour {
     public float steering = 150f;
     public float acceleration = 75f;
 
+    public Text lapCounter;
+    public GameObject endPanel;
+    public Text endText;
+
+    public int laps = 0;
+    public bool[] checkpoints = new bool[4];
+
     private Rigidbody2D _rb;
 
-	// Use this for initialization
-	void Start () {
+    void Start ()
+    {
+        Time.timeScale = 1f;
         _rb = GetComponent<Rigidbody2D>();
-	}
+        lapCounter.text = laps + " / 3";
+    }
+
+    public void AddLap()
+    {
+        laps++;
+        lapCounter.text = laps + " / 3";
+    }
+
+    public void Win()
+    {
+        endText.text = name + " win !";
+        endPanel.SetActive(true);
+        Time.timeScale = 0f;
+    }
 
     private void FixedUpdate()
     {
-        _rb.velocity = Vector2.zero;
+        if(Time.timeScale > 0f)
+        {
+            _rb.velocity = Vector2.zero;
 
-        if (Input.GetKey(leftKey))
-        {
-            _rb.rotation += steering;
-        }
-        else if (Input.GetKey(rightKey))
-        {
-            _rb.rotation -= steering;
-        }
+            if (Input.GetKey(leftKey))
+            {
+                _rb.rotation += steering;
+            }
+            else if (Input.GetKey(rightKey))
+            {
+                _rb.rotation -= steering;
+            }
 
-        if(Input.GetKey(upKey))
-        {
-            _rb.velocity = acceleration * transform.up;
-        }
-        else if (Input.GetKey(downKey))
-        {
-            _rb.velocity = acceleration * -transform.up;
+            if (Input.GetKey(upKey))
+            {
+                _rb.velocity = acceleration * transform.up;
+            }
+            else if (Input.GetKey(downKey))
+            {
+                _rb.velocity = acceleration * -transform.up;
+            }
         }
     }
 }
---
+++ Assets/Game22_CheckpointBehavior.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Game22_CheckpointBehavior : MonoBehaviour {
+    public int index;
+    public bool isEnd = false;
+    
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            print("Checkpoint");
+
+            Game22_CarController player = collision.gameObject.GetComponent<Game22_CarController>();
+            player.checkpoints[index] = true;
+
+            if(player.checkpoints.All(e => e == true) && isEnd)
+            {
+                player.AddLap();
+
+                if(player.laps == 3)
+                {
+                    player.Win();
+                }
+
+                // Set all checkpoints to false
+                player.checkpoints.ToList().ForEach(e => e = false);
+            }
+        }
+    }
+}
